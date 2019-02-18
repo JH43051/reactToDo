a9d95925efb4bdf722e9f3f2b1241618986bbdb9
John Holler <jh43051@gmail.com>
2019-02-18 16:39:20 -0500

    build added

---
+++ build/precache-manifest.48d6dcb41132c7e3ac422e26f7e41073.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "540e7c47118df31528bf",
+    "url": "/static/js/main.540e7c47.chunk.js"
+  },
+  {
+    "revision": "c7bbdf1ed5316b42f8a3",
+    "url": "/static/js/2.c7bbdf1e.chunk.js"
+  },
+  {
+    "revision": "540e7c47118df31528bf",
+    "url": "/static/css/main.6da4eb1c.chunk.css"
+  },
+  {
+    "revision": "87322132394829626a09d211c4b6a4bb",
+    "url": "/index.html"
+  }
+];
